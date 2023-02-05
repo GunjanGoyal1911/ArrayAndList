5348d1e2d298898b5a2ab313d21059c7dd7dc547
Gunjan <gunjangoyal1911@gmail.com>
2023-02-05 17:22:45 +0100

    Practice

--- ArrayMethods/Program.cs
+++ ArrayMethods/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,44 @@ namespace ArrayMethods
     {
         static void Main(string[] args)
         {
+            var numbers = new int[] { 4,2,6,9,13,1};
+
+            //Length Method
+            Console.WriteLine("Length of array is: "+ numbers.Length);
+
+            //IndexOf()
+            var index= Array.IndexOf(numbers, 13);
+            Console.WriteLine("Index of 13: " +index);
+
+            //Clear()
+            Array.Clear(numbers, 3, 2);
+            Console.WriteLine("Effect of clear():");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            //Copy()
+            var another = new int[4];
+            Array.Copy(numbers, another, 4);
+
+            Console.WriteLine("Effect of copy():");
+            foreach (var n in another)
+                Console.WriteLine(n);
+
+
+            //Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of sort():");
+            foreach(var n in numbers)
+                Console.WriteLine(n);
+
+            //Reverse()
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse():");
+            foreach(var n in numbers)
+                Console.WriteLine(n);
+
+            Console.ReadLine(); 
         }
     }
 }
---
+++ ListMethods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1,2,3,4,5};
+            //Add()
+            numbers.Add(1);
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+
+            //AddRange()
+            numbers.AddRange(new int[3] { 6, 7, 8 });
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+
+            //Indexof()
+            Console.WriteLine();
+            Console.WriteLine("Index of 1:" + numbers.IndexOf(1));
+            Console.WriteLine("Index of 1:" + numbers.LastIndexOf(1));
+
+            //Count
+            Console.WriteLine("Count :" + numbers.Count);
+
+            //Remove
+            numbers.Remove(1);
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+
+            //Want to remove all 1 from list(Cant use foreach loop if do, get exception....so using for loop)
+            for(var i=0; i< numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                    numbers.Remove(numbers[i]);
+                    
+            }
+            foreach(var number in numbers)
+                Console.WriteLine(number);
+            //clear()
+            numbers.Clear();
+            Console.WriteLine("Count :" + numbers.Count);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
